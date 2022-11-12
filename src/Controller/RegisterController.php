1c28303a70d60fb204874780e609b592ccc03506
ilan990 <77548546+ilan990@users.noreply.github.com>
2022-11-12 10:41:03 +0100

    encodage du mot de passe

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -16,14 +17,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'register')]
-    public function index(Request $request,EntityManagerInterface $entityManager): Response
+    public function index(Request $request,EntityManagerInterface $entityManager,UserPasswordHasherInterface $encode): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class,$user);
         $form->handleRequest($request);
+        $user = $form->getData();
+        $plaintextPassword = $user->getPassword();
+
 
         if ($form->isSubmitted() && $form->isValid())
         {
+            //Hash le mot de passe avant l'entrée en base de données
+            $hashedPassword = $encode->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+
+            $user->setPassword($hashedPassword);
             $entityManager->persist($user);
             $entityManager->flush();
         }
