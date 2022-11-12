1d648e034bc9dbe5b55bd77b57474f6150160c86
ilan990 <77548546+ilan990@users.noreply.github.com>
2022-11-12 11:18:19 +0100

    Inscription: contrainte au niveau du prénom

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -19,6 +20,7 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname',TextType::class,[
                 'label'=>'Prénom',
+                'constraints' => new length(10,2,30),
                 'attr' =>[
                     'placeholder'=>'Merci de renseigner votre prénom'
                 ]
